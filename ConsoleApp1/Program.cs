7c49b85da21a0a9af2a72fc2669af71c84f5766c
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-07-16 09:00:37 +0300

    Fields

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// New start
+﻿// The project is entirely meant to create a fully deployable desktop app
+
+using System;
+
+namespace IMS
+{
+    class Program
+    {
+        public const string DayofSendingBill = "Monday";
+        public int CustomerId;
+        public string FirstName;
+        public string LastName;
+        //public keyword means it can be accessed by an outside class
+       
+    }
+    class NewCustomer
+    {
+        static void main(string[] args)
+        {
+            //Using the Program odject fields in our New customer object by calling it 
+            var customer1 = new Program();
+            customer1.FirstName = "John";
+        }
+        
+    }
+}
