568e74fbc2b7804740235fee6abb179765571f19
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-08-07 15:22:07 +0300

    adding enums

--- TestOne/Program.cs
+++ TestOne/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+using System;
+
+
+// learning aboout enum type
+[Flags]
+public enum Days
+{
+ None = 0b_0000_0000, // 0
+ Monday = 0b_0000_0001, // 1
+ Tuesday = 0b_0000_0010, // 2
+ Wednesday = 0b_0000_0100, // 4
+ Thursday = 0b_0000_1000, // 8
+ Friday = 0b_0001_0000, // 16
+ Saturday = 0b_0010_0000, // 32
+ Sunday = 0b_0100_0000, // 64
+ Weekend = Saturday | Sunday
+}
+
 
 namespace MyNamespace
 {
@@ -74,6 +91,12 @@ namespace MyNamespace
             double radius = 1.2;
             double Area = Math.PI * radius * radius;
             Console.WriteLine(Area);
+            Days Today = Days.Monday;
+            Console.WriteLine(Today);
+         
+            
+            
+
 
 
         }
