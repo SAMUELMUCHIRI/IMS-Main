c6af2f1c7d1f7d043649082ff2e25114d88ecbc7
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-08-22 08:13:31 +0300

    Continuation from boolean operators

--- Chapter 3/ChapterProjects/BooleanOperators/Program.cs
+++ Chapter 3/ChapterProjects/BooleanOperators/Program.cs
@@ -11,17 +11,25 @@ namespace BooleanOper
             WriteLine($" And  |   a  |   b   ");
             WriteLine($"  a   |{a&a,-5} |{a&b,-5}");
             WriteLine($"  b   |{b&a,-5} |{b&b,-5}");
-            WriteLine($" Or  |   a  |   b   ");
+            WriteLine($" Or   |   a  |   b   ");
             WriteLine($"  a   |{a|a,-5} |{a|b,-5}");
             WriteLine($"  b   |{b|a,-5} |{b|b,-5}");
             WriteLine($" Xor  |   a  |   b   ");
             WriteLine($"  a   |{a^a,-5} |{a^b,-5}");
             WriteLine($"  b   |{b^a,-5} |{b^b,-5}");
+            WriteLine($" a & Dostuff() = {a&Dostuff()}");
+            WriteLine($" b & Dostuff() = {b&Dostuff()}");
+            
 
 
 
             
 
+        }
+        private static bool Dostuff()
+        {
+            Console.WriteLine("am going to do stuff");
+            return true;
         }
     }
 
