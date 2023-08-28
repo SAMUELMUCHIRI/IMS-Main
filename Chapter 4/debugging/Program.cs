94d871d181f52f7ab2e941c1874b42437c55535d
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-08-28 16:04:51 +0300

    debug intro

--- Chapter 4/debugging/Program.cs
+++ Chapter 4/debugging/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+namespace Debugging
+    {
+        class Program
+        {
+            static double Add(double a, double b)
+                {
+                return a + b; // deliberate bug!
+                }
+                static void Main(string[] args)
+                {
+                double a = 4.5; // or use var
+                double b = 2.5;
+                double answer = Add(a, b);
+                WriteLine($"{a} + {b} = {answer}");
+                ReadLine(); // wait for user to press ENTER
+                }
+        }
+    }
+                    
