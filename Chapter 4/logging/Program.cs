58cc260abd884f9f699c26add559659fde02fab8
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-08-29 17:27:09 +0300

    Debug and Trace Class

---
+++ Chapter 4/logging/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+namespace Instrumenting
+{
+     class Program
+         {
+         static void Main(string[] args)
+             {
+            // write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(
+            File.CreateText("log.txt")));
+            // text writer is buffered, so this option calls
+            // Flush() on all listeners after writing
+            Trace.AutoFlush = true;
+
+            Debug.WriteLine("Debug says, I am watching!");
+             Trace.WriteLine("Trace says, I am watching!");
+             }
+         }
+}
