64d69a99f75bc064ef4ac018b762c3908e7c7f60
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-09-04 13:53:30 +0300

    logging

--- Chapter 4/logging/Program.cs
+++ Chapter 4/logging/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.IO;
 using Microsoft.Extensions.Configuration;
+
 namespace Instrumenting
 {
      class Program
@@ -17,7 +18,16 @@ namespace Instrumenting
             Trace.AutoFlush = true;
 
             Debug.WriteLine("Debug says, I am watching!");
-             Trace.WriteLine("Trace says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json",optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var ts = new TraceSwitch( displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+            configuration.GetSection("PacktSwitch").Bind(ts);
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
+
              }
          }
 }
