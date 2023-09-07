dde7c03cdb001275c085ac5fa4393a06c5538785
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-09-07 18:59:10 +0300

    Chapter 5 : Instantiating a class

---
+++ Chapter 5/PeopleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using static System.Console;
+using Packt.Shared;
+
+namespace Welcpme
+{
+    public class Goat
+    {
+        static void Main(String[] args)
+        {
+        var bob = new Person();
+        WriteLine(bob.ToString());
+        }
+
+    }
+}
+
