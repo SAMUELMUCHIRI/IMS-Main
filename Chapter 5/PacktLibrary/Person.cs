051f83941e853505ed0f3237b0f6acba7a6ca74d
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-09-08 15:52:40 +0300

    Storing data within fields

--- Chapter 5/PacktLibrary/Person.cs
+++ Chapter 5/PacktLibrary/Person.cs
@@ -2,6 +2,19 @@
 namespace Packt.Shared
 {
     public class Person
+    //the code above means the class Person inherits properties from the base class object
     {
+        // all user defined class are derived from the base class Objest 
+        // fields
+    public string Name;
+    public DateTime DateOfBirth;
+
+    public WondersOfTheAncientWorld FavoriteAncientWonder;
+
+    public WondersOfTheAncientWorld BucketList;
+
     }
+  
+
+
 }
--- Chapter 5/PeopleApp/Program.cs
+++ Chapter 5/PeopleApp/Program.cs
@@ -6,11 +6,46 @@ namespace Welcpme
 {
     public class Goat
     {
-        static void Main(String[] args)
-        {
-        var bob = new Person();
-        WriteLine(bob.ToString());
-        }
+    static void Main(String[] args)
+    {
+    var bob = new Person();
+    // WriteLine(bob.ToString());
+     
+    //var bob = new Person();
+    bob.Name = "Bob Smith";
+    bob.DateOfBirth = new DateTime(1965, 12, 22);
+    Console.WriteLine(
+    format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: bob.Name,
+    arg1: bob.DateOfBirth);
+
+    var alice = new Person
+    {
+        Name = "Alice Jones",
+        DateOfBirth = new DateTime(1998, 3, 7)
+    };
+    WriteLine(
+        format: "{0} was born on {1:dd MMM yy}",
+        arg0: alice.Name,
+        arg1: alice.DateOfBirth);
+
+
+        bob.FavoriteAncientWonder = 
+        WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+        WriteLine(format: 
+        "{0}'s favorite wonder is {1}. It's integer is {2}.",
+        arg0: bob.Name,
+        arg1: bob.FavoriteAncientWonder,
+        arg2: (int)bob.FavoriteAncientWonder);
+
+        bob.BucketList = 
+        WondersOfTheAncientWorld.HangingGardensOfBabylon
+        | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+        // bob.BucketList = (WondersOfTheAncientWorld)18; 
+        WriteLine($"{bob.Name}'s bucket list is {bob.BucketList}");
+    }
+    
+        
 
     }
 }
