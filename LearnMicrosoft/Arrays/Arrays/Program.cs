340b134c7f13e7a6a764cdfaaac115675f138cb4
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-09-13 23:01:14 +0300

    Microsoft learn

---
+++ LearnMicrosoft/Arrays/Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items=orderStream.Split(",");
+Array.Sort(items);
+
+foreach (var item in items)
+{
+   
+    if ((item.Length)==4)
+    {
+        Console.WriteLine(item);
+        
+    }
+    else
+    {
+        Console.WriteLine(item +"\t- Error");
+    }
+    
+}
