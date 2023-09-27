05f2053ebad680f55b1c87e54dd5b2c23b11f027
SAMUEL <82397563+SAMUELMUCHIRI@users.noreply.github.com>
2023-09-27 15:04:47 +0300

    small changes

--- Learning Section/Chapter 4/debugging/Program.cs
+++ Learning Section/Chapter 4/debugging/Program.cs
@@ -1,4 +1,6 @@
-﻿using static System.Console;
+﻿using System;
+
+using static System.Console;
 namespace Debugging
     {
         class Program
@@ -9,11 +11,26 @@ namespace Debugging
                 }
                 static void Main(string[] args)
                 {
-                double a = 4.5; // or use var
-                double b = 2.5;
-                double answer = Add(a, b);
-                WriteLine($"{a} + {b} = {answer}");
-                ReadLine(); // wait for user to press ENTER
+                Console.WriteLine("Enter a number to add");
+                string input=Console.ReadLine();
+                
+                try
+                { 
+                    double result;
+                    double.TryParse(input , out result);
+                    double a = 4.5; // or use var
+                    double b = result;
+                    double answer = Add(a, b);
+                    WriteLine($"{a} + {b} = {answer}");
+                    ReadLine(); // wait for user to press ENTER
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+
+                }
+               
                 }
         }
     }
